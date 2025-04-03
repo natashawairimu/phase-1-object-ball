d535254d2a85275ef64129e609d060d7995223cd
natashanyokabi <natashawairimu27@gmail.com>
2025-04-03 10:22:52 +0300

    object

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,5 +1,5 @@
 function gameObject() {
-  const home = {
+  let home = {
     teamName: "Brooklyn Nets",
     colors: ["Black", "White"],
     players: {
@@ -13,9 +13,7 @@ function gameObject() {
         blocks: 1,
         slamDunks: 1,
       },
-    },
 
-    players: {
       "Reggie Evans": {
         Number: 30,
         shoe: 14,
@@ -26,9 +24,7 @@ function gameObject() {
         blocks: 12,
         slamDunks: 7,
       },
-    },
 
-    players: {
       "Brook Lopez": {
         Number: 11,
         shoe: 17,
@@ -39,9 +35,7 @@ function gameObject() {
         blocks: 1,
         slamDunks: 15,
       },
-    },
 
-    players: {
       "Mason Plumlee": {
         Number: 1,
         shoe: 19,
@@ -52,9 +46,7 @@ function gameObject() {
         blocks: 8,
         slamDunks: 5,
       },
-    },
 
-    players: {
       "Jason Terry": {
         Number: 31,
         shoe: 15,
@@ -67,7 +59,8 @@ function gameObject() {
       },
     },
   };
-  const away = {
+
+  let away = {
     teamName: "Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
     players: {
@@ -82,59 +75,53 @@ function gameObject() {
         slamDunks: 2,
       },
 
-      players: {
-        "Bismark Biyombo": {
-          Number: 0,
-          shoe: 16,
-          points: 12,
-          redbounds: 4,
-          assists: 7,
-          steals: 7,
-          blocks: 15,
-          slamDunks: 10,
-        },
+      "Bismark Biyombo": {
+        Number: 0,
+        shoe: 16,
+        points: 12,
+        redbounds: 4,
+        assists: 7,
+        steals: 7,
+        blocks: 15,
+        slamDunks: 10,
+      },
 
-        players: {
-          "DeSagna Diop": {
-            Number: 2,
-            shoe: 14,
-            points: 24,
-            redbounds: 12,
-            assists: 12,
-            steals: 4,
-            blocks: 5,
-            slamDunks: 5,
-          },
+      "DeSagna Diop": {
+        Number: 2,
+        shoe: 14,
+        points: 24,
+        redbounds: 12,
+        assists: 12,
+        steals: 4,
+        blocks: 5,
+        slamDunks: 5,
+      },
 
-          players: {
-            "Ben Gordon": {
-              Number: 8,
-              shoe: 15,
-              points: 33,
-              redbounds: 3,
-              assists: 2,
-              steals: 1,
-              blocks: 1,
-              slamDunks: 0,
-            },
+      "Ben Gordon": {
+        Number: 8,
+        shoe: 15,
+        points: 33,
+        redbounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slamDunks: 0,
+      },
 
-            players: {
-              "Brendan Haywood": {
-                Number: 33,
-                shoe: 15,
-                points: 6,
-                redbounds: 12,
-                assists: 12,
-                steals: 22,
-                blocks: 5,
-                slamDunks: 12,
-              },
-            },
-          },
-        },
+      "Brendan Haywood": {
+        Number: 33,
+        shoe: 15,
+        points: 6,
+        redbounds: 12,
+        assists: 12,
+        steals: 22,
+        blocks: 5,
+        slamDunks: 12,
       },
     },
   };
-  return gameObject()
+
+  return {home, away};
 }
-console.log(gameObject())
+console.log(gameObject());
+
